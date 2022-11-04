c79ef3af86517b198a292482e23531ebeae5a98d
Adilzhan Aubakir <aubakirof@gmail.com>
2022-11-04 18:14:45 +0600

    fix: docker and redis

--- telegram-bot.py
+++ telegram-bot.py
@@ -163,7 +163,7 @@ if "HEROKU" in list(os.environ.keys()):
     server = Flask(__name__)
 
 
-    @server.route("/bot/", methods=['POST'])
+    @server.route(f"{API_TOKEN}", methods=['POST'])
     def getMessage():
         r = request.get_data().decode("utf-8")
         bot.process_new_updates([telebot.types.Update.de_json(r)])
@@ -177,8 +177,8 @@ if "HEROKU" in list(os.environ.keys()):
 
     bot.remove_webhook()
     bot.set_webhook(
-        url=config.WEBHOOK_URL)
-    server.run(host="0.0.0.0", port=os.environ.get("PORT",8000), debug=True)
+        url=config.WEBHOOK_URL+API_TOKEN)
+    server.run(host="0.0.0.0", port=os.environ.get("PORT", 8000), debug=True)
 
 else:
     print("polling")
